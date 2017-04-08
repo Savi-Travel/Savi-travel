d8f175e2129236f8e9b42a3aa9d62350d9bef410
Alex Moores <moores.alexd@gmail.com>
2017-04-08 15:11:20 -0400

    Basic React-Native app

---
+++ components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Text
+} from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Text> This is some text </Text>
+    );
+  }
+}
+
+export default App;
--- index.ios.js
+++ index.ios.js
@@ -11,22 +11,12 @@ import {
   Text,
   View
 } from 'react-native';
+import App from './components/app';
 
 export default class SaviTravel extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <App />
     );
   }
 }
