64b7a0918541035021b2811923ecf92753501060
Ignacio Palma <naxopalma@gmail.com>
2017-04-11 19:28:46 -0300

    tourDetails basic components

---
+++ styles/styles.js
@@ -0,0 +1,20 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  locationPage: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10
+  },
+  bodyText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 5
+  }
+});
+
+module.exports = {
+  styles: styles
+}
