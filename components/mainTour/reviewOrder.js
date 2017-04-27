f65f6f674404a2142c0bce396c823300c89ed6d2
Vandeth Nop <vnop14@gmail.com>
2017-04-27 16:00:12 -0700

    ReviewOrder component code cleanup - WIP

--- components/mainTour/reviewOrder.js
+++ components/mainTour/reviewOrder.js
@@ -12,6 +12,8 @@ import {
   TouchableHighlight,
   Dimensions
 } from 'react-native';
+import Styles from '../../styles/styles.js';
+import Load from '../../node_modules/react-native-loading-gif';
 
 class ReviewOrder extends Component {
   constructor(props) {
