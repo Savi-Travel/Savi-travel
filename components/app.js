a1970dc81cca915b00e892f0f00d59891fafa325
Vandeth Nop <vnop14@gmail.com>
2017-04-26 23:36:30 -0700

    Homepage component cleaned

--- components/app.js
+++ components/app.js
@@ -20,7 +20,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      currentPage: 7,
+      currentPage: 0,
       logged: false,
       profile: '',
       token: '',
--- components/mainTour/homePage.js
+++ components/mainTour/homePage.js
@@ -13,6 +13,9 @@ import {
   ScrollView
 } from 'react-native';
 
+let {width, height} = Dimensions.get('window');
+let port = 8080;
+
 class HomePage extends Component {
   constructor(props) {
     super(props);
@@ -20,15 +23,13 @@ class HomePage extends Component {
   }
 
   componentWillMount() {
-    fetch('https://savi-travel.com:8080/api/cities')
+    fetch(`https://savi-travel.com:${port}/api/cities`)
       .then(resp => resp.json())
       .then(data => this.setState({data}))
       .catch(err => console.error(err));
   }
 
   render() {
-    let {width, height} = Dimensions.get('window');
-    let port = 8080;
     let imgUri = `https://savi-travel.com:${port}/api/images/`;
     return (
       <View>
@@ -38,19 +39,19 @@ class HomePage extends Component {
               return (
                 <View key={i}>
                   <TouchableHighlight
-                    onPress={() => {this.props.nav(1, item)}}
+                    onPress={() => { this.props.nav(1, item); }}
                   >
-                    <Image source={{uri: imgUri + item.mainImage}}
+                    <Image
+                      source={{uri: imgUri + item.mainImage}}
                       style={{width: width, height: height / 3, margin: 0}}
                     >
                     <View style={Styles.home('cityTitlesView')}>
                       <Text style={Styles.home('cityTitlesText')}>{item.name}</Text>
                     </View>
                     </Image>
-
                   </TouchableHighlight>
                 </View>
-              )
+              );
             })}
         </ScrollView>
       </View>
